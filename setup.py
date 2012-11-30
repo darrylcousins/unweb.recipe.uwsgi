e338952e3d8d2280cdf5d121a75898d5e9cb59db
Dimitris Moraitis <dimo@unweb.me>
2012-11-30 17:26:20 +0200

    rename package, prepare release

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, find_packages
 
-version = '0.0.3'
+version = '0.1'
 
 setup(
-    name = "eqb.recipe.uwsgi",
+    name = "unweb.recipe.uwsgi",
     version = version,
     description = """\
-            this is a recipe to build a uwsgi binary as well as the xml config file.
-            modified version of https://github.com/shaunsephton/shaunsephton.recipe.uwsgi
-
+            This is a recipe to build a uwsgi binary as well as the xml config file.
+            It's a modified version of https://github.com/psychotechnik/eqb.recipe.uwsgi
+            which is in turn a modified version of 
+            https://github.com/shaunsephton/shaunsephton.recipe.uwsgi
     """,
     classifiers=[
     'Framework :: Buildout',
@@ -22,15 +23,15 @@ setup(
     #package_dir={'': 'eqb/recipe/uwsgi'},
     packages=find_packages(),
     keywords='buildout, uwsgi',
-    author='Philip Kalinsky',
-    author_email='philip.kalinsky@eloquentbits.com',
-    url='https://github.com/psychotechnik/eqb.recipe.uwsgi',
+    author='Dimitris Moraitis',
+    author_email='dimo@unweb.me',
+    url='https://github.com/unweb/unweb.recipe.uwsgi',
     license='BSD',
     zip_safe=False,
     install_requires=[
         'zc.buildout',
     ],
     entry_points = {
-        'zc.buildout': ['default = eqb.recipe.uwsgi:UWSGI']
+        'zc.buildout': ['default = unweb.recipe.uwsgi:UWSGI']
     },
     )
